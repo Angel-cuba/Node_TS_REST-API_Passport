f669a8b1de5ae4e644c53540a29818cb3d45202a
Mac_Developer <araozangel842@gmail.com>
2022-05-19 10:36:04 +0300

    Server running with tsc-watch

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan'
+import cors from 'cors'
+
+//initializations
+const app = express()
+
+
+//settings
+app.set('port', process.env.PORT || 3001)
+
+//middlewares
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+export default app
