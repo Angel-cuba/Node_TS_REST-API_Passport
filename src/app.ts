f6d6b45fa95833cc0701479e83cf9f016d0cc214
Mac_Developer <araozangel842@gmail.com>
2022-05-20 11:01:18 +0300

    Using passport middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,11 @@ import express from 'express';
 import morgan from 'morgan'
 import cors from 'cors'
 import 'dotenv/config';
+import passport from 'passport';
+import passportMiddleware from './middlewares/passport';
+
 import authRoutes from './routes/auth.routes'
+import privateRoutes from './routes/private.routes'
 
 //initializations
 const app = express()
@@ -15,12 +19,17 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
+//Including passport as middlewares
+app.use(passport.initialize())
+passport.use(passportMiddleware)
+
 
 //routes
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 app.use(authRoutes)
+app.use(privateRoutes)
 
 
 export default app
